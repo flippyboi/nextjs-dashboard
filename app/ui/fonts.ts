335eeb02a326e9c418d8f4099f6451b49a5564bc
Fedor <flippybeats@gmail.com>
2023-12-29 14:45:01 +0300

    chapter 1-5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin', 'cyrillic'] });
+export const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+    plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    arrowParens: 'avoid',
+    printWidth: 100,
+    semi: true,
 };
